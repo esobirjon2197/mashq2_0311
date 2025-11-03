13258366526da1feb6bd2714db4ffec4e1b2c3e9
esobirjon2197 <esobirjon2197@gmail.com>
2025-11-03 18:01:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,48 @@
-# # 1-m
-# kop = 1
-#
-# for i in range(1, 6):
-#     kop *= i
-#
-# print(kop)
+# 1-m
+kop = 1
+
+for i in range(1, 6):
+    kop *= i
+
+print(kop)
 
 
-# # 2-m
-# kop = 1
-#
-# for i in range(1, 11):
-#     if i % 2 == 0:
-#         kop *= i
-#
-# print(kop)
+# 2-m
+kop = 1
+
+for i in range(1, 11):
+    if i % 2 == 0:
+        kop *= i
+
+print(kop)
 
 
-# # 3-m
-# kop = 1
-#
-# n = int(input("Son kirit: "))
-# for i in range(1,n):
-#     kop *= i
-#
-# print(kop)
+# 3-m
+kop = 1
+
+n = int(input("Son kirit: "))
+for i in range(1,n):
+    kop *= i
+
+print(kop)
 
 
-# # 4-m
-# kop = 1
-#
-# for i in range(3, 10):
-#     kop *= i
-#
-# print(kop)
+# 4-m
+kop = 1
+
+for i in range(3, 10):
+    kop *= i
+
+print(kop)
 
 
-# # 5-m
-# kop = 1
-# 
-# for i in range(1, 8):
-#     if i % 2 != 0:
-#         kop *= i
-# 
-# print(kop)
+# 5-m
+kop = 1
+
+for i in range(1, 8):
+    if i % 2 != 0:
+        kop *= i
+
+print(kop)
 
 
